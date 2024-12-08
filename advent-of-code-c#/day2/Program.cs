4c241e24b1dd15b884702bc03eaf77e9b4ba77d1
anhnlh <anh.lh.nguyen42@gmail.com>
2024-12-08 15:52:13 -0500

    Initial commit

---
+++ advent-of-code-c#/day2/Program.cs
@@ -0,0 +1,17 @@
+﻿// safe if both are true
+//  - either all increasing or decreasing (monotonic)
+//  - adjacent levels differ by at least 1 and at most 3
+
+
+try
+{
+    var lines = File.ReadLines("input1.txt");
+    foreach (var line in lines)
+    {
+
+    }
+}
+catch (IOException e)
+{
+    System.Console.WriteLine(e.Message);
+}

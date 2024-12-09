02b4e129d1852aa03493a84a80ae9369ebad47cf
anhnlh <anh.lh.nguyen42@gmail.com>
2024-12-08 20:09:04 -0500

    cleanup day3 solution

--- advent-of-code-c#/sol/Day3.cs
+++ advent-of-code-c#/sol/Day3.cs
@@ -7,9 +7,9 @@ internal static class Day3
     /**
      * find a number in the string and return it
      */
-    private static int ParseInt(string memory, ref int i, ref int lenMul)
+    private static int ParseInt(string memory, ref int i, ref int lenParse)
     {
-        if (i >= memory.Length - lenMul - 1 || !char.IsDigit(memory[i]))
+        if (i >= memory.Length - lenParse - 1 || !char.IsDigit(memory[i]))
         {
             i += 1;
             return -1;
@@ -19,7 +19,7 @@ internal static class Day3
         while (i < memory.Length && char.IsDigit(memory[i]))
         {
             i++;
-            lenMul++;
+            lenParse++;
         }
 
         return int.Parse(memory.Substring(startIdx, i - startIdx));
@@ -28,13 +28,13 @@ internal static class Day3
     /**
      * find a character in the string
      */
-    private static bool CharDoesntExist(string memory, char c, ref int i, ref int lenMul)
+    private static bool CharDoesntExist(string memory, char c, ref int i, ref int lenParse)
     {
-        return i >= memory.Length - lenMul++ || memory[i++] != c;
+        return i >= memory.Length - lenParse++ || memory[i++] != c;
     }
 
     /**
-     * parse "mul(a, b)" and sum up a * b
+     * parse "mul(a,b)" and sum up a * b
      */
     public static void First()
     {
@@ -91,60 +91,59 @@ internal static class Day3
             var i = 0;
             while (i < memory.Length)
             {
-                var lenMul = 0;
+                var lenParse = 0;
+                // need temp vars for i and lenMul because we don't want to advance them if parsing "don't()" fails
+                var tempI = i;
+                var tempLenParse = lenParse;
+
                 if (disabled)
                 {
-                    // parse "do()", if successful, resume parsing
+                    // parse "do()": if successful, resume parsing "mul(a,b)", else continue parsing "do()"
                     // advancing i and lenMul here is ok
-                    if (CharDoesntExist(memory, 'd', ref i, ref lenMul)) continue;
-                    if (CharDoesntExist(memory, 'o', ref i, ref lenMul)) continue;
-                    if (CharDoesntExist(memory, '(', ref i, ref lenMul)) continue;
-                    if (CharDoesntExist(memory, ')', ref i, ref lenMul)) continue;
+                    if (CharDoesntExist(memory, 'd', ref i, ref lenParse)) continue;
+                    if (CharDoesntExist(memory, 'o', ref i, ref lenParse)) continue;
+                    if (CharDoesntExist(memory, '(', ref i, ref lenParse)) continue;
+                    if (CharDoesntExist(memory, ')', ref i, ref lenParse)) continue;
                     disabled = false;
                 }
                 else
                 {
-                    // need temp vars for i and lenMul because we don't want to advance them if parsing fails
-                    var tempI = i;
-                    var tempLenMul = lenMul;
-                    if (CharDoesntExist(memory, 'd', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, 'o', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, 'n', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, '\'', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, 't', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, '(', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
-                    if (CharDoesntExist(memory, ')', ref tempI, ref tempLenMul)) goto mulParsing;
-                    i++;
+                    // parse "don't()": if successful, disable parsing, else continue parsing "mul(a,b)"
+                    if (CharDoesntExist(memory, 'd', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, 'o', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, 'n', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, '\'', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, 't', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, '(', ref tempI, ref tempLenParse)) goto mulParsing;
+                    if (CharDoesntExist(memory, ')', ref tempI, ref tempLenParse)) goto mulParsing;
+                    i = tempI;
                     disabled = true;
                     continue;
                 }
-                
+
                 mulParsing:
-                // check for 'mul('
-                if (CharDoesntExist(memory, 'm', ref i, ref lenMul)) continue;
-                if (CharDoesntExist(memory, 'u', ref i, ref lenMul)) continue;
-                if (CharDoesntExist(memory, 'l', ref i, ref lenMul)) continue;
-                if (CharDoesntExist(memory, '(', ref i, ref lenMul)) continue;
+                // advance i to tempI because of "don't()" parsing. Also need to decrement i by 1 since
+                // tempI advances by one in CharDoesntExist, but don't want to advance i here
+                i = tempI - 1;
+                // look for 'mul('
+                if (CharDoesntExist(memory, 'm', ref i, ref lenParse)) continue;
+                if (CharDoesntExist(memory, 'u', ref i, ref lenParse)) continue;
+                if (CharDoesntExist(memory, 'l', ref i, ref lenParse)) continue;
+                if (CharDoesntExist(memory, '(', ref i, ref lenParse)) continue;
 
                 // look for digits
-                var a = ParseInt(memory, ref i, ref lenMul);
+                var a = ParseInt(memory, ref i, ref lenParse);
                 if (a == -1) continue;
 
                 // look for ','
-                if (CharDoesntExist(memory, ',', ref i, ref lenMul)) continue;
+                if (CharDoesntExist(memory, ',', ref i, ref lenParse)) continue;
 
                 // look for digits
-                var b = ParseInt(memory, ref i, ref lenMul);
+                var b = ParseInt(memory, ref i, ref lenParse);
                 if (b == -1) continue;
 
                 // look for ')'
-                if (CharDoesntExist(memory, ')', ref i, ref lenMul)) continue;
+                if (CharDoesntExist(memory, ')', ref i, ref lenParse)) continue;
 
                 result += a * b;
             }
